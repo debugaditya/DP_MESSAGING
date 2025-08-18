c3992d3e10f180b826cd997a165ad151ad18c77f
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-08-18 22:58:42 +0530

    Update socket.js

--- app/socket.js
+++ app/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3001';
+const URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:8080';
 
 export const socket = io(URL, {
   autoConnect: false,
