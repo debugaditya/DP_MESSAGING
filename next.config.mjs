69d009e16b83fa0a029b9af2946c77e4530a056e
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-08-19 00:29:29 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,47 +1,6 @@
-
-// next.config.m/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-  async headers() {
-    return [
-      {
-        source: "/messages/:path*", // covers /messages and nested
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store",
-          },
-        ],
-      },
-      {
-        source: "/chatbox/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store",
-          },
-        ],
-      },
-      {
-        source: "/friends/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store",
-          },
-        ],
-      },
-      {
-        source: "/sidebar/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
