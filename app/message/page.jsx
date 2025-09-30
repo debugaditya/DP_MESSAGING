e3f9d925486746c5b3cfcc4bc5c7b6af39081262
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-09-30 23:40:47 +0530

    Update page.jsx

--- app/message/page.jsx
+++ app/message/page.jsx
@@ -1,5 +1,5 @@
 'use client';
-import './style.css';
+import './style.css'
 import { useRouter } from 'next/navigation';
 import socketService from '../socket';
 import { useState, useEffect } from 'react';
